3d0d9c61b6edd5b6612b17890ef4788c604b83f9
Robert Mads Christensen <61755754+madschristensen99@users.noreply.github.com>
2024-05-20 07:55:35 -0400

    Update BuyMeACoffeeABI.ts

--- app/_contracts/BuyMeACoffeeABI.ts
+++ app/_contracts/BuyMeACoffeeABI.ts
@@ -3,124 +3,130 @@
  * sake of minimizing bytes downloaded.
  */
 const abi = [
-  { type: 'constructor', inputs: [], stateMutability: 'nonpayable' },
-  { type: 'receive', stateMutability: 'payable' },
-  {
-    type: 'function',
-    name: 'buyCoffee',
-    inputs: [
-      { name: 'numCoffees', type: 'uint256', internalType: 'uint256' },
-      { name: 'message', type: 'string', internalType: 'string' },
-    ],
-    outputs: [],
-    stateMutability: 'payable',
-  },
-  {
-    type: 'function',
-    name: 'getMemos',
-    inputs: [
-      { name: 'index', type: 'uint256', internalType: 'uint256' },
-      { name: 'size', type: 'uint256', internalType: 'uint256' },
-    ],
-    outputs: [
-      {
-        name: '',
-        type: 'tuple[]',
-        internalType: 'struct Memo[]',
-        components: [
-          { name: 'numCoffees', type: 'uint256', internalType: 'uint256' },
-          { name: 'message', type: 'string', internalType: 'string' },
-          { name: 'time', type: 'uint256', internalType: 'uint256' },
-          { name: 'userAddress', type: 'address', internalType: 'address' },
-        ],
-      },
-    ],
-    stateMutability: 'view',
-  },
-  {
-    type: 'function',
-    name: 'memos',
-    inputs: [{ name: '', type: 'uint256', internalType: 'uint256' }],
-    outputs: [
-      { name: 'numCoffees', type: 'uint256', internalType: 'uint256' },
-      { name: 'message', type: 'string', internalType: 'string' },
-      { name: 'time', type: 'uint256', internalType: 'uint256' },
-      { name: 'userAddress', type: 'address', internalType: 'address' },
-    ],
-    stateMutability: 'view',
-  },
-  {
-    type: 'function',
-    name: 'modifyMemoMessage',
-    inputs: [
-      { name: 'index', type: 'uint256', internalType: 'uint256' },
-      { name: 'message', type: 'string', internalType: 'string' },
-    ],
-    outputs: [],
-    stateMutability: 'nonpayable',
-  },
-  {
-    type: 'function',
-    name: 'owner',
-    inputs: [],
-    outputs: [{ name: '', type: 'address', internalType: 'address payable' }],
-    stateMutability: 'view',
-  },
-  {
-    type: 'function',
-    name: 'price',
-    inputs: [],
-    outputs: [{ name: '', type: 'uint256', internalType: 'uint256' }],
-    stateMutability: 'view',
-  },
-  {
-    type: 'function',
-    name: 'removeMemo',
-    inputs: [{ name: 'index', type: 'uint256', internalType: 'uint256' }],
-    outputs: [],
-    stateMutability: 'nonpayable',
-  },
-  {
-    type: 'function',
-    name: 'setPriceForCoffee',
-    inputs: [{ name: '_price', type: 'uint256', internalType: 'uint256' }],
-    outputs: [],
-    stateMutability: 'nonpayable',
-  },
-  {
-    type: 'function',
-    name: 'withdrawTips',
-    inputs: [],
-    outputs: [],
-    stateMutability: 'nonpayable',
-  },
-  {
-    type: 'event',
-    name: 'BuyMeACoffeeEvent',
-    inputs: [
-      { name: 'buyer', type: 'address', indexed: true, internalType: 'address' },
-      { name: 'price', type: 'uint256', indexed: false, internalType: 'uint256' },
-    ],
-    anonymous: false,
-  },
-  {
-    type: 'event',
-    name: 'NewMemo',
-    inputs: [
-      { name: 'userAddress', type: 'address', indexed: true, internalType: 'address' },
-      { name: 'time', type: 'uint256', indexed: false, internalType: 'uint256' },
-      { name: 'numCoffees', type: 'uint256', indexed: false, internalType: 'uint256' },
-      { name: 'message', type: 'string', indexed: false, internalType: 'string' },
-    ],
-    anonymous: false,
-  },
-  { type: 'error', name: 'InsufficientFunds', inputs: [] },
-  {
-    type: 'error',
-    name: 'InvalidArguments',
-    inputs: [{ name: 'message', type: 'string', internalType: 'string' }],
-  },
-  { type: 'error', name: 'OnlyOwner', inputs: [] },
+	{
+		"inputs": [],
+		"name": "handleFundraiser",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_goal",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_durationInDays",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"stateMutability": "payable",
+		"type": "receive"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "balances",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "currentState",
+		"outputs": [
+			{
+				"internalType": "enum SimpleFundraiser.State",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "deadline",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "DONATION_AMOUNT",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "goal",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalRaised",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
 ] as const;
 
 export default abi;
