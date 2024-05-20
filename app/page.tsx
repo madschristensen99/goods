73b8078b9d263fec9b924b45f10f36fdb4b338f5
Robert Mads Christensen <61755754+madschristensen99@users.noreply.github.com>
2024-05-20 07:04:55 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ const frameMetadata = getFrameMetadata({
     },
     {
       action: 'tx',
-      label: 'Send Base Sepolia',
+      label: 'Contribute Base Sepolia',
       target: `${NEXT_PUBLIC_URL}/api/tx`,
       postUrl: `${NEXT_PUBLIC_URL}/api/tx-success`,
     },
