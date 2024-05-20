fbdd03872624014595b3b2c1a17affff04fefe75
Robert Mads Christensen <61755754+madschristensen99@users.noreply.github.com>
2024-05-20 07:56:30 -0400

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
 // use NODE_ENV to not have to change config based on where it's deployed
 export const NEXT_PUBLIC_URL =
   process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://zizzamia.xyz';
-export const BUY_MY_COFFEE_CONTRACT_ADDR = '0xcD3D5E4E498BAb2e0832257569c3Fd4AE439dD6f';
+export const BUY_MY_COFFEE_CONTRACT_ADDR = '0x3f01ef9b47235F388Ae1D943644BA52F76d01A63';
